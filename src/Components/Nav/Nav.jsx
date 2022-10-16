254445fc0cb500e956dfaa7b0a29122343ee3dc9
Luigi T <luigitejada@yahoo.com>
2022-10-16 09:40:34 -0400

    add routes and navigation

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="navigation-bar">
-
+      <NavLink to="/" id="logo"><img src={Logo} alt="logo" /></NavLink>
+      <NavLink to="/burgers">BURGER SHOP</NavLink>
     </nav>
   )
 }
